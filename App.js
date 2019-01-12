4d5209825b46e9013e3377622be95d57e0f85d66
farantesrodrigues <farantesrodrigues@gmail.com>
2019-01-12 16:37:10 +0100

    initial commit - departing from the original react native starter project to include react-native-elements library and vector-icons. Also adds (uncommitted) firebase app connection files.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
-
+import { SocialIcon, Input } from 'react-native-elements';
 import firebase from 'react-native-firebase';
 
 export default class App extends React.Component {
@@ -10,8 +11,7 @@ export default class App extends React.Component {
   }
 
   async componentDidMount() {
-    // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
+    const { user } = await firebase.auth().signInAnonymously();
     // console.warn('User -> ', user.toJSON());
 
     // await firebase.analytics().logEvent('foo', { bar: '123'});
@@ -21,42 +21,36 @@ export default class App extends React.Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-          <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
+          <Text style={styles.title}>
+            DR JONES
           </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          {Platform.OS === 'ios' ? (
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
+
+          <Input
+            placeholder='INPUT WITH CUSTOM ICON'
+            leftIcon={
+              <Icon
+                name='user'
+                size={24}
+                color='black'
+              />
+            }
+          />
+          <Input
+            placeholder='INPUT WITH ERROR MESSAGE'
+            errorStyle={{ color: 'red' }}
+            errorMessage='ENTER A VALID ERROR HERE'
+          />
+
+          <SocialIcon
+            title='Sign In With Facebook'
+            button
+            type='facebook'
+          />
+          <SocialIcon
+            title='Sign In With Google'
+            button
+            type='google-plus-official'
+          />
         </View>
       </ScrollView>
     );
@@ -66,37 +60,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  logo: {
-    height: 120,
+  title: {
     marginBottom: 16,
     marginTop: 64,
-    padding: 10,
-    width: 135,
-  },
-  welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
   }
 });
--- ios/DrJonesTests/DrJonesTests.m
+++ ios/DrJonesTests/DrJonesTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface DrJonesTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation DrJonesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
